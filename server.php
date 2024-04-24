0fca498514f0d0bf584389091284cb88022e4226
tamerelmenoufi <tamerelmenoufi@gmail.com>
2024-04-24 21:48:28 +0200

    Atualizações

--- server.php
+++ server.php
@@ -7,16 +7,20 @@ $server = new Server('0.0.0.0', 9501);
 
 // Evento de conexão
 $server->on('open', function (Server $server, $request) {
-    echo "Nova conexão: {$request->fd}\n";
+    //echo "Nova conexão: {$request->fd}\n";
 });
 
 // Evento de mensagem
 $server->on('message', function (Server $server, $frame) {
-    echo "Mensagem recebida: {$frame->data}\n";
+    //echo "Mensagem recebida: {$frame->data}\n";
     $conexoes = $server->connections;
-    
-    // Envie a mensagem de volta para o cliente
-    $server->push($frame->fd, json_encode(['type' => 'chat', 'message' => "{$frame->data}"]));
+    $origem = $frame->fd;
+
+    foreach($conexoes as $conxao){
+        // Envie a mensagem de volta para o cliente
+        if($conxao == $origem) continue;
+        $server->push($conxao, json_encode(['type' => 'chat', 'text' => $frame->data]));
+    }
 });
 
 // Evento de fechamento da conexão
